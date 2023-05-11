a7244b7d635298e22009b969e33fed43eb6b5f1e
Anahit Sultanova <soltonanna@gmail.com>
2023-05-11 12:36:15 +0400

    add new project

---
+++ README.md
@@ -0,0 +1,17 @@
+<img src="ProjScreen.png" alt="Add to Cart" />
+
+## Food Order
+This application is a straightforward implementation that allows users to add products to a cart using React and Redux. It utilizes Firebase's Realtime Database as the underlying database. 
+
+## Install
+
+```sh
+npm init
+```
+
+```sh
+npm start
+```
+
+## Demo
+<a href="https://soltonanna.github.io/add-to-cart-redux/" target="_blank"> Demo </a>
---
+++ src/App.js
@@ -0,0 +1,55 @@
+import { Fragment, useEffect } from 'react';
+import Cart from './components/Cart/Cart';
+import Layout from './components/Layout/Layout';
+import Products from './components/Shop/Products';
+import Notification from './components/UI/Notification';
+
+import { sendCartData, fetchCartData } from './store/cart-actions';
+
+import { useSelector, useDispatch } from 'react-redux';
+
+let isInitial = true;
+
+function App() {
+  const dispatch = useDispatch();
+
+  const showCart = useSelector(state => state.ui.cartIsVisible);
+  const cart = useSelector(state => state.cart);
+  const notification = useSelector(state => state.ui.notification);
+
+  useEffect(()=> {
+    dispatch(fetchCartData());
+  }, []);
+
+  useEffect(() => {
+    /** Not send data when page load at first */
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+    
+    if (cart.changed) {
+      dispatch(sendCartData(cart));
+    }
+    
+  }, [cart, dispatch]);
+
+  return (
+    <Fragment>
+      { notification && (
+        <Notification 
+          status={notification.status} 
+          title={notification.title} 
+          message={notification.message} 
+        />
+      )}
+
+      <Layout>
+        { showCart && <Cart /> }
+        <Products />
+      </Layout>
+    </Fragment>
+  );
+}
+
+export default App;
